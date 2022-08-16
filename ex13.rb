8ab96c8f4658bb5f09f7881d333ce15dee231de0
Aakash prabhas <66468043+aakashprabhas@users.noreply.github.com>
2022-08-16 13:23:33 +0530

    Add files via upload

--- ex13.rb
+++ ex13.rb
@@ -12,9 +12,9 @@
 #study drills
  puts " \t Study drills"
  first , second , third =ARGV
- first =" Aakash"
- second =" aakash"
- third ='Aakash.P'
+ first =" Apple "
+ second =" Orange"
+ third ="Mango"
 
 
 
@@ -22,20 +22,3 @@
  puts "YOurs first variable is: #{first}"
  puts " Yours Second Varaible is:#{second}"
  puts "Yours Third variable is:#{third}"
-
-
- #getting from user
-
-
- first , second , third =ARGV
-
- puts "yours first Varaible is :#{first}"
- gets.chomp
-
- puts"YOurs second varaible is :#{second}"
- gets.chomp
-
- puts "your third varaible is :#{ third}"
- gets.chomp
-
- puts"yours number is #{first},#{second},#{third}"
